97620e142b46ed0ac741f5b9ac4fdec841a78b7e
gunyy <gunzamiku1@gmail.com>
2024-02-06 09:51:33 +0700

    modified:   src/app/components/project/project.component.css 	modified:   src/app/components/project/project.component.html 	modified:   src/app/components/project/project.component.ts 	new file:   src/assets/pj1.png 	new file:   src/assets/pj2.png 	new file:   src/assets/pj3.jpg 	new file:   src/assets/pj4.jpg

--- src/app/components/project/project.component.ts
+++ src/app/components/project/project.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-project',
   templateUrl: './project.component.html',
-  styleUrls: ['./project.component.css']
+  styleUrls: ['./project.component.css'],
+  animations: [
+    trigger('fadeInOut', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('1s ease-in-out', style({ opacity: 1 }))
+      ])
+    ])
+  ]
 })
 export class ProjectComponent {
 
