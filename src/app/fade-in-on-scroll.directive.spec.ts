d72e85da7176b591121e8de9710fb4d26f6959c8
gunyy <gunzamiku1@gmail.com>
2024-02-06 13:51:20 +0700

    Auto-generated commit

--- src/app/fade-in-on-scroll.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FadeInOnScrollDirective } from './fade-in-on-scroll.directive';
-
-describe('FadeInOnScrollDirective', () => {
-  it('should create an instance', () => {
-    const directive = new FadeInOnScrollDirective();
-    expect(directive).toBeTruthy();
-  });
-});
